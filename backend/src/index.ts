69b4db3bfbb137f35e67b240506ebd808a59d51d
Ashish Dabral <ashishdabral2014@gmail.com>
2025-08-11 14:01:58 +0530

    fixing CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,6 +27,15 @@ app.use(
       }
     },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.options(
+  "*",
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
   })
 );
 
